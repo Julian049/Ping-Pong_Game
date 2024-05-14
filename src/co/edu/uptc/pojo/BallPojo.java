169d4c8981aec92db902afbe67e1edc8f6986c31
Diego Alejandro Gil <diego.gil04@uptc.edu.co>
2024-05-14 14:41:35 -0500

    Add Pojos classes.

---
+++ src/co/edu/uptc/pojo/BallPojo.java
@@ -0,0 +1,32 @@
+package co.edu.uptc.pojo;
+import java.awt.*;
+
+public class BallPojo {
+    private Point point;
+    private int size;
+    private int angle;
+
+
+    
+    public Point getPoint() {
+        return point;
+    }
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+    public int getSize() {
+        return size;
+    }
+    public void setSize(int size) {
+        this.size = size;
+    }
+    public int getAngle() {
+        return angle;
+    }
+    public void setAngle(int angle) {
+        this.angle = angle;
+    }
+
+
+
+}
---
+++ src/co/edu/uptc/pojo/RacketPojo.java
@@ -0,0 +1,38 @@
+package co.edu.uptc.pojo;
+import java.awt.*;
+
+public class RacketPojo {
+    private Point point;
+    private int width;
+    private int height;
+    private int speed;
+
+
+    
+    public Point getPoint() {
+        return point;
+    }
+    public void setPoint(Point point) {
+        this.point = point;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+
+}
