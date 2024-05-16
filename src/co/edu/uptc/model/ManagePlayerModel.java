da575585aad8cd599528729516dc72f167a00374
Julian <julianb0904@gmail.com>
2024-05-16 16:57:01 -0500

    Add the view of the players

---
+++ src/co/edu/uptc/model/ManagePlayerModel.java
@@ -0,0 +1,8 @@
+package co.edu.uptc.model;
+
+import java.net.Socket;
+
+public class ManagePlayerModel {
+    private Socket socket;
+
+}
---
+++ src/co/edu/uptc/pojo/PlayerPojo.java
@@ -0,0 +1,7 @@
+package co.edu.uptc.pojo;
+
+public class PlayerPojo {
+    private RacketPojo racketPojo;
+    private boolean spectator = false;
+
+}
